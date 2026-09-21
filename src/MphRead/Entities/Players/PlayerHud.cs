8cf2fada6d4ae7df77c87f21b1731acd990ecd61
Anti <213526732+AntiNotAnti@users.noreply.github.com>
2026-09-21 14:25:26 -0500

    Fix native reticle crosshair namespace

--- src/MphRead/Entities/Players/PlayerHud.cs
+++ src/MphRead/Entities/Players/PlayerHud.cs
@@ -1497,7 +1497,7 @@ namespace MphRead.Entities
                                     else
                                     {
                                         _targetCircleInst.Alpha = Features.ReticleOpacity;
-                                        _scene.DrawHudObject(_targetCircleInst, scale: Crosshair.Scale);
+                                        _scene.DrawHudObject(_targetCircleInst, scale: Mods.Render.Crosshair.Scale);
                                     }
                                     float hitMarker = Mods.Network.NetHitPrediction.MarkerAlpha;
                                     if (hitMarker > 0)
