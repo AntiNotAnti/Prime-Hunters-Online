6f47390078f1b1a3954c2fc2d9fefa3ae1881a9f
Anti <213526732+AntiNotAnti@users.noreply.github.com>
2026-09-25 18:48:02 -0500

    Track body head and flick landing state

--- src/MphRead/Mods/Input/AimAssist/AimAssistState.cs
+++ src/MphRead/Mods/Input/AimAssist/AimAssistState.cs
@@ -6,8 +6,14 @@ namespace MphRead.Mods.Input.AimAssist
     {
         public AimAssistTrackingState TrackingState;
         public Vector2 PreviousStick, FlickDirection;
-        public float FlickPeak, FlickAge, SecondsSinceLookIntent, TrackingConfidence;
-        public bool FlickActive, FlickConsumed;
+        public Vector2 StickHistory0, StickHistory1, StickHistory2, StickHistory3;
+        public Vector2 PreviousRaw, PreviousCameraVelocity;
+        public float FlickPeak, FlickSpeed, FlickAge, SecondsSinceLookIntent;
+        public float BodyTrackingConfidence, HeadTrackingConfidence;
+        public float TrackingConfidence { get => BodyTrackingConfidence; set => BodyTrackingConfidence = value; }
+        public float ShotCommitSeconds, PreviousClosingSpeed;
+        public bool FlickActive, FlickConsumed, FlickBraking, PreviousFiring;
+        public bool PreviousInsideBody, PreviousInsideHead;
         public int FlickTarget = -1;
         public int TargetSlot = -1;
         public long TargetLife;
@@ -16,13 +22,28 @@ namespace MphRead.Mods.Input.AimAssist
         public Vector2 PreviousError, PreviousHeadError, PreviousOutput;
         public Vector2 AngularVelocity, HeadAngularVelocity;
         public Vector2 AngularAcceleration, HeadAngularAcceleration;
+        public Vector2 MotionDirection, HeadMotionDirection;
+        public AimAssistMotionPhase MotionPhase;
+
+        public void PushStick(Vector2 sample)
+        {
+            StickHistory3 = StickHistory2;
+            StickHistory2 = StickHistory1;
+            StickHistory1 = StickHistory0;
+            StickHistory0 = sample;
+        }
 
         public void Reset()
         {
             TrackingState = AimAssistTrackingState.None;
             PreviousStick = FlickDirection = default;
-            FlickPeak = FlickAge = SecondsSinceLookIntent = TrackingConfidence = 0;
-            FlickActive = FlickConsumed = false;
+            StickHistory0 = StickHistory1 = StickHistory2 = StickHistory3 = default;
+            PreviousRaw = PreviousCameraVelocity = default;
+            FlickPeak = FlickSpeed = FlickAge = SecondsSinceLookIntent = 0;
+            BodyTrackingConfidence = HeadTrackingConfidence = 0;
+            ShotCommitSeconds = PreviousClosingSpeed = 0;
+            FlickActive = FlickConsumed = FlickBraking = PreviousFiring = false;
+            PreviousInsideBody = PreviousInsideHead = false;
             FlickTarget = -1;
             TargetSlot = -1;
             TargetLife = 0;
@@ -31,6 +52,8 @@ namespace MphRead.Mods.Input.AimAssist
             PreviousError = PreviousHeadError = PreviousOutput = default;
             AngularVelocity = HeadAngularVelocity = default;
             AngularAcceleration = HeadAngularAcceleration = default;
+            MotionDirection = HeadMotionDirection = default;
+            MotionPhase = AimAssistMotionPhase.None;
         }
     }
 }
