3ae90a8c2f44b28859defcb582a305b0ab11eb28
Anti <213526732+AntiNotAnti@users.noreply.github.com>
2026-09-24 23:42:44 -0500

    Keep analog replay state in decoder intents

--- src/MphRead/Mods/Replay/ReplayWorldSchemas.cs
+++ src/MphRead/Mods/Replay/ReplayWorldSchemas.cs
@@ -816,9 +816,6 @@ internal static class ReplayWorldSchemas
         ["MphRead.Entities.PlayerControls"] = [
             "<MouseAim>k__BackingField",
             "<KeyboardAim>k__BackingField",
-            "AnalogMoveActive",
-            "AnalogMoveX",
-            "AnalogMoveY",
             "<MoveLeft>k__BackingField",
             "<MoveRight>k__BackingField",
             "<MoveUp>k__BackingField",
